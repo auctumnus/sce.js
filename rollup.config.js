b3062a99396ee6c9f910b2f15a8802a420854ff2
auctumnus <auctumnus@users.noreply.github.com>
2020-05-19 16:33:22 -0500

    chore: uglify to try to make builds smaller

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
 import pkg from './package.json'
 
 export default [
@@ -14,7 +15,8 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      babel({ babelHelpers: 'bundled' })
+      babel({ babelHelpers: 'bundled' }),
+      uglify()
     ]
   },
   {
