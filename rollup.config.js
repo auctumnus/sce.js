0f1e924b136b5ed70efaba2d473080314323bce5
auctumnus <auctumnus@users.noreply.github.com>
2020-08-26 18:20:23 -0500

    chore: fix vulnerability warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 export default [
@@ -16,7 +16,7 @@ export default [
       resolve(),
       commonjs(),
       babel({ babelHelpers: 'bundled' }),
-      uglify()
+      terser()
     ]
   },
   {
