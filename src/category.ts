f508795e350ed9bc916b6b51aec5b41b0fde125c
auctumnus <auctumnus@pm.me>
2022-09-25 19:04:55 -0500

    ✨ category.transform

--- src/category.ts
+++ src/category.ts
@@ -1,8 +1,28 @@
+/**
+ * Represents an SCE category.
+ */
 export class Category {
   constructor(public elements: string[], public name?: string) {}
 
+  /**
+   * Whether this category contains the given phone.
+   */
   has(item: string) {
     return this.elements.includes(item)
+  }
+
+  /**
+   * The index of the given phone.
+   */
+  index(item: string) {
+    return this.elements.indexOf(item)
+  }
+
+  /**
+   * The element at the given index.
+   */
+  at(index: number) {
+    return this.elements.at(index)
   }
 
   toString() {
@@ -10,5 +30,29 @@ export class Category {
       (this.name ? `Category "${this.name}": ` : 'Anonymous category: ') +
       this.elements.join(', ')
     )
+  }
+
+  /**
+   * Transforms a phone from one category to another. The new phone has the same
+   * index, modulo the length of the target category. For example, given the
+   * ruleset:
+   * ```
+   * A = a,b,c,d,e,f
+   * B = g,h,i
+   * [A] > [B]
+   * ```
+   * the word `abcdef` would be transformed into `ghighi`.
+   * @param targetCategory
+   * @param phone
+   * @returns
+   */
+  transform(targetCategory: Category, phone: string) {
+    const ourIndex = this.index(phone)
+    if (ourIndex === -1) {
+      return phone
+    } else {
+      const index = ourIndex % targetCategory.elements.length
+      return targetCategory.at(index)
+    }
   }
 }
